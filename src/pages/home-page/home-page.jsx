2ec7ea9351896c306c87abd08f8b9891ff1b5268
Alex Charland <ac3charland@gmail.com>
2021-11-16 15:46:44 -0600

    Add description & links to GitHub

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -5,6 +5,7 @@ import FilmTile from '../../components/film-tile/film-tile'
 
 import './home-page.scss'
 import {StoreContext} from '../../app'
+import {BACKEND_CODE, FRONTEND_CODE, GITHUB_URL} from '../../utils/constants'
 
 const cb = 'home'
 
@@ -14,7 +15,11 @@ const HomePage = () => {
 
     return (
         <div className={cb} style={{backgroundColor: state.backgroundColor}}>
-            <h1 className={`${cb}__heading`}>GhibliQL</h1>
+            <div className={`${cb}__title-wrapper`}>
+                <h1 className={`${cb}__heading`}>GhibliQL</h1>
+                <h2 className={`${cb}__description`}>A GraphQL-powered catalog by <a href={GITHUB_URL}>Alex Charland</a></h2>
+                <h2 className={`${cb}__repos`}><a href={FRONTEND_CODE}>Frontend Code</a><a href={BACKEND_CODE}>Backend Code</a></h2>
+            </div>
             <div className={`${cb}__tiles-wrapper`}>
                 {data && data.films.map((film, i) => (
                     <FilmTile key={`film-${i}`} {...film} />
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,10 @@
 export const SECONDARY_PAGE_URL = '/secondary'
 
+export const GITHUB_URL = 'https://github.com/ac3charland'
+
+export const BACKEND_CODE = 'https://github.com/ac3charland/graphql-practice'
+export const FRONTEND_CODE = 'https://github.com/ac3charland/graphql-react-practice'
+
 // Redux Actions
 export const MARK_HOME_PAGE_AS_VISITED = 'MARK_HOME_PAGE_AS_VISITED'
 
